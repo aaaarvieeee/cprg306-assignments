3354d319a277fd5ee02205caa073c50bce99307d
aaaarvieeee <119615693+aaaarvieeee@users.noreply.github.com>
2023-09-15 07:30:43 -0600

    updated page.js
    - fixed incorrect routing for the week2 page

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col gap-8 p-24 justify-center">
       <h1 className='text-3xl pb-3'>CPRG 306: Web Development 2 - Assignments</h1>
       <StudentInfo />
-      <Link className="hover:underline" href="\page.js">Week 2</Link>
+      <Link className="hover:underline" href="\week2">Week 2</Link>
     </main>
   )
 }
